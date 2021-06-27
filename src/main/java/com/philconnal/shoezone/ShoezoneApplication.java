7143b75b355d7d0582e7b675f8b8de69ab0820a1
Phicnq2411 <phicnq2411@gmail.com>
2021-06-27 13:06:25 +0700

    initial

---
+++ src/main/java/com/philconnal/shoezone/ShoezoneApplication.java
@@ -0,0 +1,13 @@
+package com.philconnal.shoezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoezoneApplication.class, args);
+	}
+
+}
